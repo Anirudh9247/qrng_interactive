23d1f0e2328449851dd9ade0cf7a0c8d030dfb2d
Anirudh <anirudhsriramulu92@gmail.com>
2026-03-04 22:10:15 +0530

    Implement user authentication with JWT token handling and current user retrieval

--- QRNG_PROJECT/backend/app/core/auth.py
+++ QRNG_PROJECT/backend/app/core/auth.py
@@ -1,5 +1,11 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.model.user import User
 
 SECRET_KEY = "supersecretkey"   # Later move to .env
 ALGORITHM = "HS256"
@@ -10,3 +16,23 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub") # type: ignore
+
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
